2c095cfe0da676408f7206ddd2225c985fef15eb
WilfredoN <wilfredochannel@gmail.com>
2024-03-12 14:49:42 +0200

    .env created, tested

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const API_URL = process.env.REACT_APP_API_URL;
+
   return (
     <div className="App">
       <header className="App-header">
@@ -12,6 +14,14 @@ function App() {
           <br />
           This is Meduzzen internship project.
         </p>
+        <a
+          className="App-link"
+          href={API_URL + 'docs#'}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn more
+        </a>
       </header>
     </div>
   );
