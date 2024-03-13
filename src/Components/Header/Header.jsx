0f8aa9554130523cb0c14e8e2f09cde057501ea3
WilfredoN <wilfredochannel@gmail.com>
2024-03-13 22:06:25 +0200

    Header component, Material-UI

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,14 +1,67 @@
 import React from 'react';
 import { Link, BrowserRouter } from 'react-router-dom';
+import { AppBar, Toolbar, Container, Button } from '@mui/material';
+
 import './Header.css';
 const Header = () => {
 	return (
 		<BrowserRouter>
-			<nav>
-				<Link to="/about">About</Link>
-				<Link to="/users">List of users</Link>
-				<Link to="/companies">List of companies</Link>
-			</nav>
+			<AppBar
+				position="static"
+				sx={{
+					boxShadow: 'none',
+					backgroundColor: 'transparent',
+					mt: 2,
+				}}
+			>
+				<Container
+					maxWidth="lg"
+					sx={(theme) => ({
+						display: 'flex',
+						alignItems: 'center',
+						justifyContent: 'space-between',
+						'& a': {
+							margin: theme.spacing(0, 2),
+							color: theme.palette.common.white,
+							textDecoration: 'none',
+							fontSize: '1.4rem',
+						},
+						borderRadius: '100px',
+						backgroundColor: theme.palette.primary.main,
+					})}
+				>
+					<Toolbar variant="regular">
+						<Link to="/about">About</Link>
+						<Link to="/users">Users</Link>
+						<Link to="/companies">Companies</Link>
+					</Toolbar>
+					<Toolbar>
+						<Link to="/profile">Profile</Link>
+						<Button
+							variant="contained"
+							color="primary"
+							sx={{
+								boxShadow: 'none',
+								borderRadius: '10px',
+								textTransform: 'none',
+							}}
+						>
+							Sign Up
+						</Button>
+						<Button
+							variant="contained"
+							color="primary"
+							sx={{
+								boxShadow: 'none',
+								borderRadius: '10px',
+								textTransform: 'none',
+							}}
+						>
+							Sign In
+						</Button>
+					</Toolbar>
+				</Container>
+			</AppBar>
 		</BrowserRouter>
 	);
 };
