9fa13945d9fc314573dee59035507705845ea2bb
WilfredoN <wilfredochannel@gmail.com>
2024-03-16 00:50:32 +0200

    Revert "Add Redux store and state management"
    This reverts commit 9c7373ea92ec3db13d6998eb3d0a34373c183fab.

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,29 @@
-import React, { Suspense, lazy, useState } from 'react';
-import { Provider, useDispatch, useSelector } from 'react-redux';
-import './App.css';
-import { store } from './Store/store';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Header from './Components/Header/Header';
+import React from 'react';
 import logo from './logo.svg';
-import { RootState } from './Store/store';
-import { setStringValue } from './Store/StringSlice';
-const About = lazy(() => import('./Pages/About/About'));
-const Users = lazy(() => import('./Pages/Users/Users'));
-const Companies = lazy(() => import('./Pages/Companies/Companies'));
-const UserProfile = lazy(() => import('./Pages/User_Profile/UserProfile'));
-const Register = lazy(() => import('./Pages/Registration/Registration'));
-const Login = lazy(() => import('./Pages/Login/Login'));
-function App() {
-  //const API_URL = process.env.REACT_APP_API_URL;
-  const dispatch = useDispatch();
-  const [testString, setTestString] = useState('Init Test Value');
+import './App.css';
 
-  const handleStringChange = (newValue: string) => {
-    setTestString(newValue);
-  };
+function App() {
+  const API_URL = process.env.REACT_APP_API_URL;
+  console.log('API_URL:', API_URL);
   return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <div className="App">
-          <Header />
-          <Suspense
-            fallback={<img src={logo} alt="logo" className="App-logo z-10" />}
-          >
-            <Routes>
-              <Route path="/" element={<About />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/users" element={<Users />} />
-              <Route path="/companies" element={<Companies />} />
-              <Route path="/profile" element={<UserProfile />} />
-              <Route path="/register" element={<Register />} />
-              <Route path="/login" element={<Login />} />
-            </Routes>
-          </Suspense>
-          <p>Test String: {testString}</p>
-          <button
-            className="bg-slate-500 rounded-full p-6"
-            onClick={() =>
-              handleStringChange(`New Test Value ${Math.random() * 1000}`)
-            }
-          >
-            Change Test String
-          </button>
-        </div>
-      </BrowserRouter>
-    </Provider>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hi!
+          <br />
+          This is Meduzzen internship project.
+        </p>
+        <a
+          className="App-link"
+          href={API_URL + 'docs#'}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn more
+        </a>
+      </header>
+    </div>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
-import { store } from './Store/store';
+//import dotenv from 'dotenv';
+
+//dotenv.config();
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>
 );
 
