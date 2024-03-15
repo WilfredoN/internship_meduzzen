0a696df044b59baf87062c53955fb102b8b4b2f9
WilfredoN <wilfredochannel@gmail.com>
2024-03-15 22:56:21 +0200

    Add Redux store and state management

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
-import React, { Suspense, lazy } from 'react';
+import React, { Suspense, lazy, useState } from 'react';
+import { Provider, useDispatch, useSelector } from 'react-redux';
 import './App.css';
+import { store } from './Store/store';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './Components/Header/Header';
 import logo from './logo.svg';
+import { RootState } from './Store/store';
+import { setStringValue } from './Store/StringSlice';
 const About = lazy(() => import('./Pages/About/About'));
 const Users = lazy(() => import('./Pages/Users/Users'));
 const Companies = lazy(() => import('./Pages/Companies/Companies'));
@@ -10,27 +14,43 @@ const UserProfile = lazy(() => import('./Pages/User_Profile/UserProfile'));
 const Register = lazy(() => import('./Pages/Registration/Registration'));
 const Login = lazy(() => import('./Pages/Login/Login'));
 function App() {
-  const API_URL = process.env.REACT_APP_API_URL;
+  //const API_URL = process.env.REACT_APP_API_URL;
+  const dispatch = useDispatch();
+  const [testString, setTestString] = useState('Init Test Value');
 
+  const handleStringChange = (newValue: string) => {
+    setTestString(newValue);
+  };
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Header />
-        <Suspense
-          fallback={<img src={logo} alt="logo" className="App-logo z-10" />}
-        >
-          <Routes>
-            <Route path="/" element={<About />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/users" element={<Users />} />
-            <Route path="/companies" element={<Companies />} />
-            <Route path="/profile" element={<UserProfile />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/login" element={<Login />} />
-          </Routes>
-        </Suspense>
-      </div>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <div className="App">
+          <Header />
+          <Suspense
+            fallback={<img src={logo} alt="logo" className="App-logo z-10" />}
+          >
+            <Routes>
+              <Route path="/" element={<About />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/users" element={<Users />} />
+              <Route path="/companies" element={<Companies />} />
+              <Route path="/profile" element={<UserProfile />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/login" element={<Login />} />
+            </Routes>
+          </Suspense>
+          <p>Test String: {testString}</p>
+          <button
+            className="bg-slate-500 rounded-full p-6"
+            onClick={() =>
+              handleStringChange(`New Test Value ${Math.random() * 1000}`)
+            }
+          >
+            Change Test String
+          </button>
+        </div>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
