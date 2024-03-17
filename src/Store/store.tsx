cfcf85e622e08ae6bcbe29951e693555f09328c1
WilfredoN <wilfredochannel@gmail.com>
2024-03-17 16:11:58 +0200

    Add Redux and React-Redux for state management, test string manipulate

---
+++ src/Store/store.tsx
@@ -0,0 +1,10 @@
+import { configureStore } from '@reduxjs/toolkit';
+import testStringReducer from './StringSlice';
+
+export const store = configureStore({
+  reducer: {
+    testString: testStringReducer,
+  },
+});
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
