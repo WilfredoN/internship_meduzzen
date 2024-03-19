a415257bbfacfeb6e0617f0af6f6c2615d8319b4
WilfredoN <wilfredochannel@gmail.com>
2024-03-20 00:04:11 +0200

    Refactor Table component to handle different data types

--- src/Components/Table/Table.tsx
+++ src/Components/Table/Table.tsx
@@ -2,26 +2,30 @@ import React from 'react';
 import TableHeader from './TableHeader';
 import Row, { CellType } from './Row';
 
+// Define the data type for the table.
+interface TablePropsType {
+  [key: string]: string | boolean | number;
+}
+// Define the table props.
 interface TableProps {
-  data: any[];
+  data: TablePropsType[];
 }
 
-const getCellValue = (key: string, value: any) => {
-  if (key.includes('avatar')) {
-    return (
-      <img
-        src={String(value)}
-        alt={key}
-        className="w-1/2 rounded-full mx-auto"
-      />
-    );
-  }
-
-  if (typeof value === 'boolean') {
-    return value ? 'Yes' : 'No';
-  }
-
-  return value;
+// value starting with http - image, so process as image;
+// value is boolean - process as Yes or No;
+// otherwise, process as is.
+const getCellValue = (key: string, value: string | boolean | number) => {
+  return typeof value === 'string' && value.startsWith('http') ? (
+    <img src={String(value)} alt={key} className="w-1/2 rounded-full mx-auto" />
+  ) : typeof value === 'boolean' ? (
+    value ? (
+      'Yes'
+    ) : (
+      'No'
+    )
+  ) : (
+    value
+  );
 };
 
 const Table: React.FC<TableProps> = ({ data }) => {
@@ -29,7 +33,7 @@ const Table: React.FC<TableProps> = ({ data }) => {
 
   const headers = Object.keys(data[0]);
 
-  const formatData = (item: any): CellType[] => {
+  const formatData = (item: TablePropsType): CellType[] => {
     return Object.entries(item).map(([key, value]) => {
       const cellValue = getCellValue(key, value);
 
