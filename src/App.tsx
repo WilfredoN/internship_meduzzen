0e7de883bbe52d7e09c79aaba0104668c61d3dee
WilfredoN <wilfredochannel@gmail.com>
2024-03-22 13:17:25 +0200

    remove unnecessary redux store

--- src/App.tsx
+++ src/App.tsx
@@ -11,8 +11,6 @@ import { useDispatch, useSelector } from 'react-redux';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 //Store
-import { RootState } from './Store/store';
-import { setStringValue } from './Store/StringSlice';
 
 //Pages
 const About = lazy(() => import('./Pages/About/About'));
@@ -22,13 +20,6 @@ const UserProfile = lazy(() => import('./Pages/User_Profile/UserProfile'));
 const Register = lazy(() => import('./Pages/Registration/Registration'));
 const Login = lazy(() => import('./Pages/Login/Login'));
 function App() {
-  const dispatch = useDispatch();
-  const testString = useSelector((state: RootState) => state.testString.value);
-  const handleButtonClick = () => {
-    dispatch(
-      setStringValue((Number(Math.random().toPrecision(1)) * 100).toString()),
-    );
-  };
   return (
     <BrowserRouter>
       <div className="App">
@@ -46,8 +37,6 @@ function App() {
             <Route path="/login" element={<Login />} />
           </Routes>
         </Suspense>
-        <p>Value: {testString} </p>
-        <button onClick={handleButtonClick}>Set Value</button>
       </div>
     </BrowserRouter>
   );
